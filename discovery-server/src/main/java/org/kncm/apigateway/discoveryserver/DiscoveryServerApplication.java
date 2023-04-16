ab5fb1babb38f338ed7ffbb5ebf4819840f74fbd
Nenad Sredojević <n.sredojevic3@gmail.com>
2023-04-16 18:50:51 +0200

    Added api gateway

--- discovery-server/src/main/java/org/kncm/apigateway/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/org/kncm/apigateway/discoveryserver/DiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package org.kncm.discoveryserver;
+package org.kncm.apigateway.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
