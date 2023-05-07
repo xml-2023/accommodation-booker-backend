d6ce099739c5a5f610c2a509f9ad45c3ad85d978
Nenad Sredojević <n.sredojevic3@gmail.com>
2023-05-07 22:28:42 +0200

    Upgraded search with date range criteria

--- accommodation-service/src/main/java/com/kncm/accommodationservice/controller/availability/AccommodationAvailabilityController.java
+++ accommodation-service/src/main/java/com/kncm/accommodationservice/controller/availability/AccommodationAvailabilityController.java
@@ -21,7 +21,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/accommodation/availability")
