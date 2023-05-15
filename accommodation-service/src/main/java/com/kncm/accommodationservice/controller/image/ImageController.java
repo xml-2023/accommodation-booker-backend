44ce2adfae215e45bf2ceaf5b012f378b8484d2c
Nenad Sredojević <n.sredojevic3@gmail.com>
2023-05-15 22:40:54 +0200

    Added method for getting images for accommodation

--- accommodation-service/src/main/java/com/kncm/accommodationservice/controller/image/ImageController.java
+++ accommodation-service/src/main/java/com/kncm/accommodationservice/controller/image/ImageController.java
@@ -1,19 +1,26 @@
 package com.kncm.accommodationservice.controller.image;
 
+import com.kncm.accommodationservice.handler.exceptions.NoAvailableImagesException;
 import com.kncm.accommodationservice.model.Image;
 import com.kncm.accommodationservice.service.image.ImageService;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/accommodation/image")
@@ -44,15 +51,23 @@ public class ImageController {
     }
 
     //read images
-//    @GetMapping("/images")
-//    public List<String> getImageUrls() throws IOException {
-//        Resource[] resources = new PathMatchingResourcePatternResolver().getResources("classpath*:images/*.*");
-//        List<String> urls = new ArrayList<>();
-//        for (Resource resource : resources) {
-//            String url = "data:" + Files.probeContentType(resource.getFile().toPath()) + ";base64," +
-//                    Base64.getEncoder().encodeToString(IOUtils.toByteArray(resource.getInputStream()));
-//            urls.add(url);
-//        }
-//        return urls;
-//    }
+    @GetMapping()
+    public List<String> getAccommodationImageUrls(@RequestParam("name") String name) throws IOException {
+        String path = "classpath:images/" + name + "/";
+        Resource[] resources;
+        try {
+            resources = new PathMatchingResourcePatternResolver().getResources(path + "*.*");
+        }
+        catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+            throw new NoAvailableImagesException();
+        }
+        List<String> urls = new ArrayList<>();
+        for (Resource resource : resources) {
+            String url = "data:" + Files.probeContentType(resource.getFile().toPath()) + ";base64," +
+                    Base64.getEncoder().encodeToString(resource.getInputStream().readAllBytes());
+            urls.add(url);
+        }
+        return urls;
+    }
 }
